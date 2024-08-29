7984b6609c0624873e82654e564cb6b0d7000637
sunwardodin <sunwardodin96@outlook.com>
2024-08-29 11:18:17 -0700

    small changes

--- forLoops/main.go
+++ forLoops/main.go
@@ -28,4 +28,28 @@ func main() {
 		}
 		fmt.Println("counting even numbers: ", i)
 	}
+
+	// Nested loop
+	for i := 0; i < 5; i++ {
+		fmt.Println("--")
+		for j := 0; j < 5; j++ {
+			fmt.Printf("outer loop %v \t inner loop %v\n", i, j)
+		}
+	}
+	fmt.Println()
+
+	// For range loops
+	xi := []int{42, 43, 44, 45, 46, 47}
+	for i, v := range xi {
+		fmt.Println("ranging over a slice", i, v)
+	}
+	fmt.Println()
+
+	m := map[string]int{
+		"James":      42,
+		"Moneypenny": 32,
+	}
+	for k, v := range m {
+		fmt.Println("ranging over a map", k, v)
+	}
 }
